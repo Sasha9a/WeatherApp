228d7cfae63842aff1fd1406b93dc37547eb8360
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-08-10 20:06:07 +0300

    finish

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Weather.Models;
+
+namespace Weather.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private static readonly string[] Direction = new[]
+        {
+            "С", "СЗ", "З", "ЮЗ", "Ю", "ЮВ", "В", "СВ"
+        };
+        private static readonly string[] Precipitation = new[]
+        {
+            "Солнечно", "Облачно", "Небольшой дождь", "Сильный дождь", "Гроза"
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<DataWeatherModel>> Get()
+        {
+            List<DataWeatherModel> data = new List<DataWeatherModel>();
+            Random rand = new Random();
+
+            for (int i = 0; i != 10; i++)
+            {
+                DataWeatherModel dataWeather = new DataWeatherModel();
+                int MinTemp = rand.Next(20, 30);
+
+                dataWeather.Date = DateTime.Now.AddDays(i).ToShortDateString();
+                dataWeather.Temperature = String.Format("+{0} (+{1})", rand.Next(MinTemp, 30), MinTemp);
+                dataWeather.Pressure = String.Format("{0} мм рт.ст.", rand.Next(730, 760));
+                dataWeather.Humidity = String.Format("{0}%", rand.Next(0, 100));
+                dataWeather.Wind = String.Format("{0} м/с, {1}", rand.Next(1, 30), Direction[rand.Next(Direction.Length)]);
+                dataWeather.Precipitation = Precipitation[rand.Next(Precipitation.Length)];
+
+                data.Add(dataWeather);
+            }
+            return data;
+        }
+    }
+}
---
+++ Models/DataWeatherModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Weather.Models
+{
+    public class DataWeatherModel
+    {
+        public string Date { get; set; }
+        public string Temperature { get; set; }
+        public string Pressure { get; set; }
+        public string Humidity { get; set; }
+        public string Wind { get; set; }
+        public string Precipitation { get; set; }
+    }
+}
